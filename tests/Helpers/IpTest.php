43642eb92034e66f125ee1a5d22f75e69439bf5b
vallabhdas kansagara <vrkansagara@gmail.com>
2024-08-22 11:43:45 +0530

    lets see what pipline has all default values

--- tests/Helpers/IpTest.php
+++ tests/Helpers/IpTest.php
@@ -7,12 +7,15 @@ namespace Vrkansagara\Common\Helpers;
 use PHPUnit\Framework\TestCase;
 
 use function getIpecho;
+use function sprintf;
 
 class IpTest extends TestCase
 {
     public function testGetIpechoIp()
     {
         $getIpechoData = getIpecho();
+
+        echo sprintf("Your current ip is %s", $getIpechoData);
 
         $this->assertIsString($getIpechoData);
     }
